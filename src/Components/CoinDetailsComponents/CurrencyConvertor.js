84a3d611a5d154e0c760cab3a69842ba32fa8f8a
Ramsonodoi <ramson.odoi@amalitech.org>
2022-03-25 15:00:29 +0000

    UseState For Currency Converter

---
+++ src/Components/CoinDetailsComponents/CurrencyConvertor.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react'
+
+const CurrencyConvertor = ({ detail, currentcurrency }) => {
+    const [from, setfrom] = useState(1)
+
+    const renderCurrencyConvertor = () => {
+        if (detail) {
+
+            
+}
