bc40e8f059abb1d623dee419ff490002a1b55406
Ramsonodoi <ramson.odoi@amalitech.org>
2022-03-25 17:45:08 +0000

    Added style and div classname watchlisticon

--- src/Components/CoinDetailsComponents/NameSection.js
+++ src/Components/CoinDetailsComponents/NameSection.js
@@ -25,6 +25,14 @@ const NameSection = ({ detail }) => {
                     <img src={detail.image} height="32" width="32" alt={detail.symbol} />
                     <h2 className=" coin-name-n-logo-container h1" color="text">
                         <div className="coinname">{detail.name}</div><small className="nameSymbol">{detail.symbol.toUpperCase()}</small></h2>
+                        <span className="watchlist-icon-container">
+                        <button className="watchlist-icon watchlist-icon-1 watchlistbtn" onClick={handleClick}>
+                                <span className="icon-Star-Filled">
+                                {" "}
+                                {(turnyellow === "true") ?  <i className="fa fa-star yellow-color " style={{color:"blue"}} ></i>   : <i className="fa fa-star"></i>}
+                                    </span>
+                                    </button>
+                    </span>
             )
         }
             
