5534975b363c5b3eaa335217576f9c00107d4218
Ramsonodoi <ramson.odoi@amalitech.org>
2022-03-25 06:58:49 +0000

    Currentcurrency

--- src/CoinDetails/CoinDetails.js
+++ src/CoinDetails/CoinDetails.js
@@ -10,3 +10,21 @@ import StateSection from './CoinDetailsComponents/StateSection'
 import CurrencyConvertor from './CoinDetailsComponents/CurrencyConvertor'
 import AddInPortfolio from './CoinDetailsComponents/AddInPortfolio'
 import Spinner from '../../assets/Spinner'
+
+
+//currentcurrency
+
+const CoinDetails = ({currentcurrency}) => {
+    const { id } = useParams();
+    const [coinData, setCoinData] = useState({});
+    const [isLoading, setIsLoading] = useState(false);
+    const [timeFormat, setTimeFormat] = useState("24h");
+  
+    const formatData = (data) => {
+      return data.map((el) => {
+        return {
+          x: new Date(el[0]),
+          y: parseFloat(el[1]),
+        };
+      });
+    };
