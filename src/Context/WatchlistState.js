7fb0b7bb999e390885ae220b1094d933804f0cbf
ntiBoateng <gideon.boateng@amalitech.org>
2022-03-25 02:21:07 -0800

    added watchlist functionality

--- src/Context/WatchlistState.js
+++ src/Context/WatchlistState.js
@@ -1,2 +1,19 @@
 import WatchlistContext from "./watchlistContext";
 import { useState } from "react";
+const WatchlistState = (props) => {
+    const host = "https://my--crypto.herokuapp.com"
+      // "https://git.heroku.com/my--crypto.git"
+    // "http://localhost:5000"
+    const watchlistsInitial = []
+    const [watchlists, setWatchlists] = useState(watchlistsInitial)
+  
+    // Get all Watchlists
+    const getWatchlists = async () => {
+      // API Call 
+      const response = await fetch(`${host}/api/watchlist/fetchallwatchlists`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          "auth-token": localStorage.getItem('token')
+        }
+      });
