cf7ca735c1582e3d60261c8913514a62fb58b5c2
ntiBoateng <gideon.boateng@amalitech.org>
2022-03-25 02:24:04 -0800

    edit a watchlist

--- src/Context/WatchlistState.js
+++ src/Context/WatchlistState.js
@@ -1,28 +1,28 @@
 import WatchlistContext from "./watchlistContext";
 import { useState } from "react";
-const WatchlistState = (props) => {
-    const host = "https://my--crypto.herokuapp.com"
-      // "https://git.heroku.com/my--crypto.git"
-    // "http://localhost:5000"
-    const watchlistsInitial = []
-    const [watchlists, setWatchlists] = useState(watchlistsInitial)
-  
-    // Get all Watchlists
-    const getWatchlists = async () => {
-      // API Call 
-      const response = await fetch(`${host}/api/watchlist/fetchallwatchlists`, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-          "auth-token": localStorage.getItem('token')
-        }
-      });
 
-      const json = await response.json() 
-      setWatchlists(json)
-    }
-  
-    
+const WatchlistState = (props) => {
+  const host = "https://my--crypto.herokuapp.com"
+    // "https://git.heroku.com/my--crypto.git"
+  // "http://localhost:5000"
+  const watchlistsInitial = []
+  const [watchlists, setWatchlists] = useState(watchlistsInitial)
+
+  // Get all Watchlists
+  const getWatchlists = async () => {
+    // API Call 
+    const response = await fetch(`${host}/api/watchlist/fetchallwatchlists`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        "auth-token": localStorage.getItem('token')
+      }
+    });
+
+    const json = await response.json() 
+    setWatchlists(json)
+  }
+
   // Add a Watchlist
   const addWatchlist = async (coinid) => {
     // TODO: API Call
@@ -40,8 +40,9 @@ const WatchlistState = (props) => {
       console.log(watchlist)
     setWatchlists(watchlists.concat(watchlist))
   }
-   // Delete a Watchlist
-   const deleteWatchlist = async (id) => {
+
+  // Delete a Watchlist
+  const deleteWatchlist = async (id) => {
     // API Call
     const response = await fetch(`${host}/api/watchlist/deletewatchlist/${id}`, {
       method: 'DELETE',
@@ -56,8 +57,8 @@ const WatchlistState = (props) => {
     setWatchlists(newWatchlists)
   }
 
-   // Edit a Watchlist
-   const editWatchlist = async (id, coinid) => {
+  // Edit a Watchlist
+  const editWatchlist = async (id, coinid) => {
     // API Call 
     const response = await fetch(`${host}/api/watchlist/updatewatchlist/${id}`, {
       method: 'PUT',
@@ -82,6 +83,7 @@ const WatchlistState = (props) => {
     }  
     setWatchlists(newWatchlists);
   }
+
   return (
     <WatchlistContext.Provider value={{ watchlists, addWatchlist, deleteWatchlist, editWatchlist, getWatchlists }}>
       {props.children}
@@ -90,4 +92,3 @@ const WatchlistState = (props) => {
 
 }
 export default WatchlistState;
-
