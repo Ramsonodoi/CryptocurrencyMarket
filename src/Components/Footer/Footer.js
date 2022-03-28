1ca4d02665e0f4e9c9856ab3e11262905ba4ef00
Ramsonodoi <ramson.odoi@amalitech.org>
2022-03-28 13:45:59 +0000

    Update the footer and added a style to it

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -6,24 +6,6 @@ const Footer = () => {
             <footer className="site-footer">
                 <div className="container">
                     <div className="row">
-                        <div className="col-sm-12 col-md-6">
-                            
-                            <p className="text-justify">
-                                <b> 
-                                <br />
-                               </b>
-                               
-                            </p>
-                        </div>
-                        <div className="col-xs-6 col-md-3">  
-                        </div>
-                        <div className="col-xs-6 col-md-3">    
-                        </div> 
-                    </div>
-                    <hr />
-                </div>
-                <div className="container">
-                    <div className="row">
                         <div className="col-md-8 col-sm-6 col-xs-12">
                             <p className="copyright-text"> <b>Copyright © 2022 All Rights Reserved by Ramson And Gideon </b> </p>
                         </div>
