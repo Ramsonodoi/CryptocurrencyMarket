9210d45fb38528b804145448e28fe1e031dbe79e
Ramsonodoi <ramson.odoi@amalitech.org>
2022-03-28 23:20:25 +0000

    Format Code

--- src/Components/IndividualCoins/Coins.js
+++ src/Components/IndividualCoins/Coins.js
@@ -1,57 +1,80 @@
-import React from 'react';
+import React from "react";
 import "./Coinsstyle.css";
 
-
-// Coins Component ThaT Shows CurrentCurrency In Dollar And Euro 
+// Coins Component ThaT Shows CurrentCurrency In Dollar And Euro
 
 const Coins = ({ coin, currentcurrency }) => {
-    
-    
-    
+  return (
+    <>
+      <tbody>
+        <tr>
+          <td>
+            <div>
+              <div className="cmc-div">
+                {" "}
+                &nbsp;{currentcurrency === "eur" ? "€" : "$"}&nbsp;{" "}
+                {coin.current_price}
+              </div>
+            </div>
+          </td>
+          <td>
+            <div>
+              <div className="cmc-div">
+                {" "}
+                &nbsp;{currentcurrency === "eur" ? "€" : "$"}&nbsp;{" "}
+                {coin.high_24h}
+              </div>
+            </div>
+          </td>
 
-    return (
-        <>
-            <tbody>
-                <tr>
-                    <td>
-                        <div >
-                            <div className="cmc-div"> &nbsp;{(currentcurrency==="eur")?"€":"$"}&nbsp; {coin.current_price}</div>
-                        </div>
-                    </td>
-                    <td>
-                        <div >
-                            <div className="cmc-div"> &nbsp;{(currentcurrency==="eur")?"€":"$"}&nbsp; {coin.high_24h}</div>
-                        </div>
-                    </td>
+          <td>
+            <div>
+              <div className="cmc-div">
+                {" "}
+                &nbsp;{currentcurrency === "eur" ? "€" : "$"}&nbsp;{" "}
+                {coin.low_24h}
+              </div>
+            </div>
+          </td>
+          <td>
+            <div>
+              <div
+                className={
+                  coin.price_change_percentage_24h < 0
+                    ? "text-danger"
+                    : "text-success"
+                }
+              >
+                {" "}
+                {coin.price_change_percentage_24h < 0 ? (
+                  <i className="fas fa-sort-down align-middle mr-3"></i>
+                ) : (
+                  <i className="fas fa-sort-up align-middle mr-1"></i>
+                )}
+                {coin.price_change_percentage_24h < 0
+                  ? Math.abs(coin.price_change_percentage_24h)
+                  : coin.price_change_percentage_24h}
+              </div>
+            </div>
+          </td>
+          <td>
+            <div>
+              <div className="cmc-div">
+                {" "}
+                &nbsp;{currentcurrency === "eur" ? "€" : "$"}&nbsp;{" "}
+                {coin.market_cap_change_24h}
+              </div>
+            </div>
+          </td>
+          <td>
+            <div>
+              <div className="cmc-div">{coin.circulating_supply}</div>
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </>
+  );
+};
 
-                    <td>
-                        <div >
-                            <div className="cmc-div">  &nbsp;{(currentcurrency==="eur")?"€":"$"}&nbsp; {coin.low_24h}</div>
-                        </div>
-                    </td>
-                    <td>
-                        <div >
-                            <div className={coin.price_change_percentage_24h < 0 ? "text-danger" : "text-success"}>
-                                {" "}
-                                {coin.price_change_percentage_24h < 0 ? <i className="fas fa-sort-down align-middle mr-3"></i>   : <i className="fas fa-sort-up align-middle mr-1"></i>}
-                                
-                                {coin.price_change_percentage_24h<0?Math.abs(coin.price_change_percentage_24h):coin.price_change_percentage_24h}</div>
-                        </div>
-                    </td>
-                    <td>
-                        <div >
-                            <div className="cmc-div">  &nbsp;{(currentcurrency==="eur")?"€":"$"}&nbsp; {coin.market_cap_change_24h}</div>
-                        </div>
-                    </td>
-                    <td>
-                        <div >
-                            <div className="cmc-div">{coin.circulating_supply}</div>
-                        </div>
-                    </td>
-                </tr>
-            </tbody>
-        </>
-    )
-}
-
-export default Coins
+export default Coins;
